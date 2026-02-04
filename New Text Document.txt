a9ad081cce7cc11eb368d2cbb2055bc1fe26c1a4
yeshaalll <yeshaanjhela@gmail.com>
2026-02-04 18:23:36 +0700

    Initial commit Cafe Shop Management System

---
+++ New Text Document.txt
@@ -0,0 +1,27 @@
+# Cafe Shop Management System ☕
+
+Aplikasi manajemen cafe berbasis Java (NetBeans).
+
+## 📌 Fitur
+- Manajemen menu cafe
+- Menampilkan produk dan harga
+- Sistem Cetak Receipt
+- Integrasi database MySQL
+
+## 🛠 Teknologi
+- Java
+- NetBeans
+- MySQL
+
+## 📁 Struktur Folder
+- src/ : source code
+- assets/images/ : gambar produk
+- database/ : file SQL
+- nbproject/ : konfigurasi NetBeans
+
+## 🗄 Database
+File database tersedia di:
+database/cafedb.sql
+
+
+-- SMALL PROJECT --
---
+++ src/cafeshopmanagementsystem/database.java
@@ -0,0 +1,22 @@
+package cafeshopmanagementsystem;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class database {
+
+    public static Connection connectDB() {
+
+        try {
+
+            Class.forName("com.mysql.jdbc.Driver");
+
+            Connection connect = DriverManager.getConnection("jdbc:mysql://localhost/cafe", "root", ""); // LINK YOUR DATABASE
+            return connect;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
